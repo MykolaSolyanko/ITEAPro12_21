6a44e544a70dac32083db6e5092502c1a39741da
ntelesheva <38777372+ntelesheva@users.noreply.github.com>
2021-12-16 20:50:34 +0200

    Update Natasha_Telesheva_HW1.cpp

--- Natasha_Telesheva_HW1.cpp
+++ Natasha_Telesheva_HW1.cpp
@@ -1,42 +1,45 @@
-
-#include <iostream>
 #include <cmath>
-using namespace std;
+#include <iostream>
 
-int main()
-{
+int main() {
     const int kConst1{ 2 };
     const int kConst2{ 4 };
+    const int minRange{ 2 };
+    const int maxRange{ 255 };
 
-    double a{}, b{}, c{}, d{}, x1{}, x2{};
-    cout <<" Please enter numbers: a,b,c in range[2..255]\n";
-    cin >> a;
+    long double a{};
+    std::cout << " Please enter numbers: a,b,c in range[2..255]\n";
+    std::cin >> a;
     if (a == 0) {
-        cout << "The equation is not quadratic" << endl;
+        std::cout << "The equation is not quadratic" << std::endl;
         return 0;
     }
-    cin>> b >> c;
-    if ((a < 2) || (b< 2) || (c< 2) || (a  > 255) || (b>255) ||(c>255) ){
-        cout << "You entered an incorrect number, try entering the number again";
+    long double b{}, c{};
+    std::cin >> b;
+    std::cin >> c;
+    if ((a < minRange) || (b < minRange) || (c < minRange) || 
+        (a > maxRange) || (b > maxRange) || (c > maxRange)) {
+        std::cout << "You entered an incorrect number, try entering the number again";
         return 0;
     }
-    cout << "Your numbers:" << a << " "<< b << " " << " " << c << endl;;
+    std::cout << "Your numbers:" << a << " " << b << " " << " " << c << std::endl;
 
-    d = b * b - kConst2 * a * c;
-   
+    const long double d{ b * b - kConst2 * a * c };
+    const long double kConst{ kConst1 * a };
+
+    long double x1{}, x2{};
     if (d > 0) {
-        x1 = ((-b) + sqrt(d)) / (kConst1 * a);
-        x2 = ((-b) - sqrt(d)) / (kConst1 * a);
-        cout << "x1 = " << x1 << "\n";
-        cout << "x2 = " << x2 << "\n";
+        x1 = ((-b) + sqrt(d)) / kConst;
+        x2 = ((-b) - sqrt(d)) / kConst;
+        std::cout << "x1 = " << x1 << "\n";
+        std::cout << "x2 = " << x2 << "\n";
     }
     if (d == 0) {
-        x1 = -(b / (kConst1 * a));
-        cout << "x1 = x2 = " << x1 << "\n";
+        x1 = -(b / kConst);
+        std::cout << "x1 = x2 = " << x1 << "\n";
     }
     if (d < 0) {
-        cout << "D < 0, There are no real roots of the equation";
+        std::cout << "D < 0, There are no real roots of the equation";
     }
-  return 0;
+    return 0;
 }
-
