409603f28d0d6ba5d3ef07ab42ef3b34d121a2cd
d-polischuk <96122452+d-polischuk@users.noreply.github.com>
2021-12-18 11:12:07 +0200

    Update HomeWork1PolishchukDariia.cpp
    внесла правки согласно вашим комментариям

--- HomeWork1PolishchukDariia.cpp
+++ HomeWork1PolishchukDariia.cpp
@@ -8,43 +8,48 @@ int main() {
   double a{};
   double b{};
   double c{};
-  double x1{};
-  double x2{};
+  const int min{0};
+  const int max{1000};
 
-  for (;;) {
-    cout << "Введите значение а:";
+  while (true) {
+    cout << "Введите значение а от 1 до 1000:";
     cin >> a;
-    if (a > 0 && a < 1000) {
+    if (a > min && a < max) {
       break;
     } else {
       cout << "Неправильный диапазон! Введите число от 1 до 1000\n";
     }
   }
 
-  for (;;) {
-    cout << "Введите значение b:";
+  while (true) {
+    cout << "Введите значение b от 1 до 1000:";
     cin >> b;
-    if (b > 0 && b < 1000) {
+    if (b > min && b < max) {
       break;
     } else {
       cout << "Неправильный диапазон! Введите число от 1 до 1000\n";
     }
   }
 
-  for (;;) {
-    cout << "Введите значение c:";
+  while (true) {
+    cout << "Введите значение c от 1 до 1000:";
     cin >> c;
-    if (c > 0 && c < 1000) {
+    if (c > min && c < max) {
       break;
     } else {
       cout << "Неправильный диапазон! Введите число от 1 до 1000\n";
     }
   }
 
-  if ((b * b - 4 * a * c) >= 0) {
-    x1 = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
+  const double d = b * b - 4 * a * c; //дискриминант
+
+  if (d >= 0) {
+    double x1{};
+    double x2{};
+
+    x1 = (-1 * b + sqrt(d)) / (2 * a);
     cout << "Первый корень: " << x1 << endl;
-    x2 = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    x2 = (-1 * b - sqrt(d)) / (2 * a);
     cout << "Второй корень: " << x2 << endl;
   } else {
     cout << "Корень несущественый. Дискриминант меньше 0" << endl;
