66a8c89dc452bdfb3ac36891e4b25ae4cba68e8d
d-polischuk <96122452+d-polischuk@users.noreply.github.com>
2021-12-22 16:50:07 +0200

    Update Polishchuk_Dariia_HW2.cpp

--- Polishchuk_Dariia_HW2.cpp
+++ Polishchuk_Dariia_HW2.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 int main() {
-  int N;
+  int size_t;
   long sum;
   std::cout << "Введите количество чисел: ";
-  std::cin >> N;
-  int a[N];
-  for (int i = 0; i < N; i++) {
+  std::cin >> size_t;
+  int a[size_t];
+  for (int i = 0; i < size_t; i++) {
     std::cout << "Введите число: ";
     std::cin >> a[i];
   }
 
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < size_t; i++) {
     sum += a[i];
   }
 
   std::cout << "Сумма ваших чисел: " << sum << std::endl;
-  float avarege = sum / N;
+  float avarege = sum / size_t;
   std::cout << "Среднее арифметическое: " << avarege << std::endl;
 
   return 0;
