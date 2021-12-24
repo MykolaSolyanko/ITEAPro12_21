20b7dd422849e1673edac28a78b9f2e9f1cba4cc
root4096 <79375025+root4096@users.noreply.github.com>
2021-12-24 23:33:53 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,43 @@
-﻿#include"Quadr_Eq.h"
-Quadr_Eq create()
-{
-	int a[3]{};
-	for (size_t i = 0; i < 3; i++)
-	{
-		cout << "Введите число " << i + 1 << ": "; cin >> a[i];
-	}
-	return Quadr_Eq(a[0], a[1], a[2]);
-}
+#include <iostream>
+constexpr int two{ 2 }, four{ 4 };// const-values
+
+ 
 int main()
 {
-	setlocale(LC_ALL, "ukr");
-
-	Quadr_Eq q(2, 12, 5);
-	q.print();
-	q = create();
-	q.print();
+	float a{}, b{}, c{};// 
+   //accept data from user
+	std::cout << "Enter A: "; std::cin >> a;
+	std::cout << "Enter B: "; std::cin >> b;
+	std::cout << "Enter C: "; std::cin >> c;
+  
+	float* res = nullptr;//result`s pointer
+	float discriminant = std::move(pow(b, two) - static_cast<double> (four )* a * c);//calculation of discriminant
+	if (discriminant < 0)
+	{
+		if (a == 0)
+		{
+			res = new float;
+			*res = b / (-c);
+			delete res;
+		}
+		else {
+			res = nullptr;
+			std::cout << "No results!\n";
+		}
+	}
+	else if (discriminant == 0)
+	{
+		res = new float;
+		*res = (-b / (two * a));
+		std::cout << "result: " << *res << '\n';
+		delete res;
+	}
+	else {
+		res = new float[2];
+		res[0] = (-b + sqrt(discriminant)) / (two * a);
+		std::cout << "result 1: " << res[0] << '\n';
+		res[1] = (-b - sqrt(discriminant)) / (two * a);
+		std::cout << "result 2: " << res[1];
+		delete[] res;
+	}
 }
