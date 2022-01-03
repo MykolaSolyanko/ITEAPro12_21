eeacd0bc1b53c1c7a4f230f63503dcdaf3edb034
d-polischuk <96122452+d-polischuk@users.noreply.github.com>
2022-01-03 11:55:17 +0200

    Update Polishchuk_Dariia_HW2(6 christmas tree).cpp

--- Polishchuk_Dariia_HW2(6 christmas tree).cpp
+++ Polishchuk_Dariia_HW2(6 christmas tree).cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 
-int main()
-{
-	int height{};
- 
-	std::cout << "Введите высоту ёлочки: ";
-	std::cin >> height;
- 
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 1; j < height - i; j++)
-		{
-			std::cout << ' ';
-		}	
- 
-		for (int j = height - 2 * i; j <= height; j++)
-		{
-			std::cout << '*';
-		}			
-		std::cout << std::endl;
-	}
-	return 0;
+int main() {
+  int height{};
+
+  std::cout << "Enter the height of the tree: ";
+  std::cin >> height;
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 1; j < height - i; j++) {
+      std::cout << ' ';
+    }
+
+    for (int j = height - 2 * i; j <= height; j++) {
+      std::cout << '*';
+    }
+    std::cout << std::endl;
+  }
+  return 0;
 }
