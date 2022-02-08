e7f83bc1fa9311fa14e67363ec1393ecb3ce6b6d
Eliseyhom <77342316+Eliseyhom@users.noreply.github.com>
2022-02-08 19:39:26 +0200

    Update AverageNumber.cpp

--- AverageNumber.cpp
+++ AverageNumber.cpp
@@ -1,23 +1,24 @@
+
 #include <iostream>
 
 int main() {
-    int a_input{};
-    std::cout<<"Enter your number:\n";
-    std::cin>>a_input;
+  int a_input{};
+  std::cout << "Enter your number:\n";
+  std::cin >> a_input;
 
-    int count{};//сумма чисел
-    int countNum{};//кол-во чисел
+  int count{};    //сумма чисел
+  int countNum{}; //кол-во чисел
 
-    while (a_input){
-    count+=a_input%10;
-    countNum+=1;
-    a_input/=10;
-}
-    std::cout<<"Sum of nubers: "<<count<<"\n";
+  while (a_input) {
+    count += a_input % 10;
+    countNum += 1;
+    a_input /= 10;
+  }
+  std::cout << "Sum of nubers: " << count << "\n";
 
-    const int averageNum{count/countNum};
-   
-    std::cout<<"Average number: "<<averageNum;
-   
-    return 0;
+  const int averageNum{count / countNum};
+
+  std::cout << "Average number: " << averageNum;
+
+  return 0;
 }
